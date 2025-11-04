b5b5938d347004986d3413bf9464b63506397c69
Rostislav <98273925+rostinator4@users.noreply.github.com>
2025-11-03 22:32:02 -0600

    added background

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import bg from './assets/signinbackground.jpg'
+
+function App() {
+
+  return (
+    <div
+      className="app-background"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+
+    </div>
+  );
+}
+
+export default App;
